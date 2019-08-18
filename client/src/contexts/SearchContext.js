c62c2573181e84bb4919d0892d774b9b93e8d2d2
hansol <onesorry49@gmail.com>
2019-08-18 17:28:56 +0900

    [#6] 입력 검색어 redis에 저장
    redis 로컬 서버에 저장

--- client/src/contexts/SearchContext.js
+++ client/src/contexts/SearchContext.js
@@ -1,17 +1,28 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import SockJS from 'sockjs-client'
+import Stomp from 'stompjs'
 
 export const SearchContext = createContext()
 
 const SearchContextProvider = props => {
+  const [stompClient, setStompClient] = useState(Stomp.over(new SockJS('http://localhost:8080/websocket')))
+
   const [naverSearch, setNaverSearch] = useState([])
   const [youtubeSearch, setYoutubeSearch] = useState([])
   const [twitterSearch, setTwitterSearch] = useState([])
+
+  useEffect(() => {
+    stompClient.connect({}, frame => {
+      console.log('Connected: ' + frame);
+    })
+  })
 
   // TODO 공통 중복 체크, input 비우기
   const addNaverSearch = (search, e) => {
     const newSearch = search.trim()
     if (!naverSearch.includes(newSearch) && newSearch !== '') {
       setNaverSearch([...naverSearch, newSearch])
+      stompClient.send("/app/naver/search", {}, search)
     }
   }
 
@@ -19,6 +30,7 @@ const SearchContextProvider = props => {
     const newSearch = search.trim()
     if (!youtubeSearch.includes(newSearch)) {
       setYoutubeSearch([...youtubeSearch, newSearch])
+      stompClient.send("/app/youtube/search", {}, search)
     }
   }
 
@@ -26,6 +38,7 @@ const SearchContextProvider = props => {
     const newSearch = search.trim()
     if (!twitterSearch.includes(newSearch)) {
       setTwitterSearch([...twitterSearch, newSearch])
+      stompClient.send("/app/twitter/search", {}, search)
     }
   }
 
--- server/src/main/java/hansol/server/controller/WebSocketController.java
+++ server/src/main/java/hansol/server/controller/WebSocketController.java
@@ -1,5 +1,6 @@
 package hansol.server.controller;
 
+import hansol.server.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,10 +10,38 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 
+    private final static String NAVER_REDIS_KEY = "naver";
+    private final static String YOUTUBE_REDIS_KEY = "youtube";
+    private final static String TWITTER_REDIS_KEY = "twitter";
+
+    private RedisService redisService;
+
+    WebSocketController(RedisService redisService) {
+        this.redisService = redisService;
+    }
+
     @MessageMapping("/hello")
     @SendTo("/topic/hello")
     public String hello(String hello) {
         log.info("this is hello() : " + hello);
         return hello;
+    }
+
+    @MessageMapping("/naver/search")
+    public void addRedis(String search) {
+        log.info("new naver search word : {}", search);
+        redisService.sadd(NAVER_REDIS_KEY, search);
+    }
+
+    @MessageMapping("/youtube/search")
+    public void addYoutubeSearch(String search) {
+        log.info("new youtube search word : {}" ,search);
+        redisService.sadd(YOUTUBE_REDIS_KEY, search);
+    }
+
+    @MessageMapping("/twitter/search")
+    public void addTwitterSearch(String search) {
+        log.info("new twitter search word : {}" ,search);
+        redisService.sadd(TWITTER_REDIS_KEY, search);
     }
 }
